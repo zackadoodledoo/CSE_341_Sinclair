aedfe088edff1db0ce0e7484000077ea2b30f0a3
zackadoodledoo <sin12014@byui.edu>
2026-02-05 11:35:11 -0800

    redo part 4 2.5.26 fingers crossed

--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,18 @@
 export function getData(req, res) {
   res.json({ message: "Hello from the controller!" });
 }
+
+const express = require(‘express’);
+const mongoose = require(‘mongoose’);
+const app = express();
+const PORT = 4000;
+app.listen(PORT,()=>{
+ console.log(`Server Running on port ${PORT}`);
+});
+
+const app = express();
+mongoose.connect(`mongodb+srv://sin12014:MangoMongoDB123@cluster0.0oo8r1p.mongodb.net/?appName=Cluster0`,(err)=>{
+if(err) throw err;
+console.log(“DB Connected Successfully”);
+});
+
